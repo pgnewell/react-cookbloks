51264619a72ad6b45f1fd0a2f6f3e3e0ecf734ea
Paul Newell <paulgnewell@gmail.com>
2017-04-18 17:36:00 -0400

    get menu click to work

--- src/MenuItem/component.js
+++ src/MenuItem/component.js
@@ -1,71 +1,76 @@
 import React, { Component, PropTypes } from 'react';
 //import { canMoveRecipe, moveRecipe } from './';
-import { ItemTypes } from '../Constants';
-import { DropTarget } from 'react-dnd';
+// import { ItemTypes } from '../Constants';
+// import { DropTarget } from 'react-dnd';
 
-const recipeTarget = {
-  canDrop(props) {
-    return true; //canMoveRecipe(props.x, props.y);
-  },
-
-  drop(props) {
-
-  }
-};
-
-function collect(connect, monitor) {
-  return {
-    connectDropTarget: connect.dropTarget(),
-    isOver: monitor.isOver(),
-    canDrop: monitor.canDrop()
-  };
-}
+// const recipeTarget = {
+//   canDrop(props) {
+//     return true; //canMoveRecipe(props.x, props.y);
+//   },
+//
+//   drop(props, monitor) {
+//     if (props.onDrop) {
+//       props.onDrop(props, monitor);
+//     }
+//   },
+// };
+//
+// function collect(connect, monitor) {
+//   return {
+//     connectDropTarget: connect.dropTarget(),
+//     isOver: monitor.isOver(),
+//     canDrop: monitor.canDrop()
+//   };
+// }
 
 class MenuItem extends Component {
 
-  renderOverlay(color) {
-    return (
-      <div style={{
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        height: '100%',
-        width: '100%',
-        zIndex: 1,
-        opacity: 0.5,
-        backgroundColor: color,
-      }} />
-    );
-  }
+  // renderOverlay(color) {
+  //   return (
+  //     <div style={{
+  //       position: 'absolute',
+  //       top: 0,
+  //       left: 0,
+  //       height: '100%',
+  //       width: '100%',
+  //       zIndex: 1,
+  //       opacity: 0.5,
+  //       backgroundColor: color,
+  //     }} />
+  //   );
+  // }
 
   render() {
-    const { connectDropTarget, isOver, canDrop } = this.props;
+    //const { connectDropTarget, isOver, canDrop } = this.props;
     var list = this.props.menu.recipes.map((m) =>
-      <li>
-        m.name
+      <li key={m.id}>
+        {  m.name}
       </li>
     );
 
-    return connectDropTarget(
-      <div style={{
-        position: 'relative',
-        width: '100%',
-        height: '100%'
-      }}>
+    // return connectDropTarget(
+    //   <div style={{
+    //     position: 'relative',
+    //     width: '100%',
+    //     height: '100%'
+    //   }}>
+    return(
+      <div>
         <p>
           {this.props.menu.name}
         </p>
         <ul>{list}</ul>
-        {isOver && canDrop && this.renderOverlay('red')}
+        {/* {isOver && canDrop && this.renderOverlay('red')} */}
       </div>
     );
   }
 }
 
-MenuItem.propTypes = {
-  isOver: PropTypes.bool.isRequired,
-  canDrop: PropTypes.bool.isRequired,
-  menu: PropTypes.object
-};
+// MenuItem.propTypes = {
+//   isOver: PropTypes.bool.isRequired,
+//   canDrop: PropTypes.bool.isRequired,
+//   menu: PropTypes.object
+// };
 
-export default DropTarget(ItemTypes.RECIPE, recipeTarget, collect)(MenuItem);
+// export default DropTarget(ItemTypes.RECIPE, recipeTarget, collect)(MenuItem);
+export default MenuItem;
--- src/Menus/component.js
+++ src/Menus/component.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 // import Dispatcher from 'flux';
-import { MenuItem } from "../MenuItem/component";
+import MenuItem from "../MenuItem/component";
 import './style.css';
 
 import { menus } from '../InMemoryData/Data';
 
 class Menus extends Component {
   handleClick(menu) {
+    console.log("here in handleClick land");
     ReactDOM.render(
       <MenuItem menu={menu}/>,
       document.getElementById('item-box')
@@ -15,12 +16,14 @@ class Menus extends Component {
   }
   render() {
     var list = menus.map( (m) =>
-      <div className="menu-item" onClick={() => this.handleClick(m)}>
+      <div className="menu-item"  key={m.id} onClick={() => this.handleClick(m)}
+        style={{cursor: 'pointer'}}>
         {m.name}
       </div>
     );
     list.push(
-      <div className="new-menu-item" onClick={() => this.handleClick(null)}>
+      <div className="new-menu-item" key={-1} onClick={() => this.handleClick(null)}
+        style={{cursor: 'pointer'}}>
         (New Menu)
       </div>
     )
